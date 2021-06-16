ef3ddfc3b42e1ea25cde6f057194a531a8064661
Mauricio A de Almeida <80648566+d2021101420@users.noreply.github.com>
2021-06-16 20:36:43 -0300

    Change to use the midair dataset

--- data_loader.py
+++ data_loader.py
@@ -6,11 +6,11 @@ from torch.utils.data import Dataset
 
 class KittiLoader(Dataset):
     def __init__(self, root_dir, mode, transform=None):
-        left_dir = os.path.join(root_dir, 'image_02/data/')
+        left_dir = os.path.join(root_dir, 'color_left/')
         self.left_paths = sorted([os.path.join(left_dir, fname) for fname\
                            in os.listdir(left_dir)])
         if mode == 'train':
-            right_dir = os.path.join(root_dir, 'image_03/data/')
+            right_dir = os.path.join(root_dir, 'color_right/')
             self.right_paths = sorted([os.path.join(right_dir, fname) for fname\
                                 in os.listdir(right_dir)])
             assert len(self.right_paths) == len(self.left_paths)
